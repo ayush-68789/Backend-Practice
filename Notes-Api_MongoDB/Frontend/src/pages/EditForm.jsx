bbbd5d78ccc1add9f8687f997b5ea2620bac4b91
Ayush Chaurasia <ayush.chaurasia68789@gmail.com>
2026-07-18 17:37:58 +0530

    Added Edit Feat and Fully Completed

--- Notes-Api_MongoDB/Frontend/src/pages/EditForm.jsx
+++ Notes-Api_MongoDB/Frontend/src/pages/EditForm.jsx
@@ -1,10 +1,64 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom"
+
 const EditForm = () => {
-  return (
-    <div>
-        <input type="text" name="title" value={}/>
-        <input type="text" name="desc" value={}/>
-    </div>
-  )
+    const [notes, setNote] = useState({
+        title : '', 
+        desc : '' 
+    })
+    const params = useParams() ; 
+    const navigate = useNavigate() ; 
+    let id = params.id ;
+
+    const prefetchData = async (id)=>{
+        const response = await fetch(`http://localhost:5050/notes/${id}`) ;
+        const data = await response.json() ;
+        setNote({
+            title : data.data.title ,
+            desc : data.data.desc
+        })
+    }
+
+    useEffect(()=>{
+        prefetchData(id) ; 
+    }, [id])
+
+    const inputChange = async (e) => {
+        setNote({
+            ...notes, 
+            [e.target.name] : e.target.value 
+        })
+    }
+
+    const updateNote = async (id) => {
+        const response = await fetch(`http://localhost:5050/notes/${id}`,
+            {
+                method : "PUT",
+                headers : {
+                    "Content-Type" : "application/json"
+                }, 
+                body : JSON.stringify(notes) 
+            }
+        )
+        const data = await response.json() ; 
+        console.log(data) ; 
+    }
+
+    const submitHandler = async(e) => {
+        e.preventDefault() ; 
+        await updateNote(id) ; 
+        navigate('/notes') ; 
+    }
+
+    return (
+        <div>
+            <form onSubmit={submitHandler}>
+                <input type="text" name="title" value={notes.title} onChange={inputChange} />
+                <input type="text" name="desc" value={notes.desc} onChange={inputChange} />
+                <button>Submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default EditForm
